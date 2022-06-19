f3e9e7be54c7871c7276044ec6389dd8aecd3efe
Alexey Pirozerskiy <pirozerskiy.a@wb.ru>
2022-06-19 11:56:59 +0300

    feat:add first group tests

---
+++ tests/test-user.ts
@@ -0,0 +1,15 @@
+import { Person } from '../src/types';
+
+export const testUser:Person = {
+    id:'',
+    username:'Alexey',
+    age:39,
+    hobbies:['travel']
+};
+
+export const changedUser:Person = {
+    id:'',
+    username:'Superman',
+    age:39,
+    hobbies:['fly']
+};
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,10 @@
-
+import { changedUser, testUser } from './test-user';
 import { start, server } from '../src/worker';
+import { Person } from '../src/types';
+
 const request = require('supertest');
+
+let user: Person | undefined;
 
 beforeEach(async () => {
     await start();
@@ -15,8 +19,49 @@ test('два плюс два равно четыре', () => {
     expect(2 + 2).toBe(4);
 });
 
-it('should be no users', async () => {
+it('Get all records with a GET api/users request (an empty array is expected)   ', async () => {
     const response = await request(server).get('/api/users/');
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([]);
 });
+
+it('A new object is created by a POST api/users request (a response containing newly created record is expected)',
+    async () => {
+
+        const response = await request(server).post('/api/users/').send(JSON.stringify(testUser));
+        if (response.statusCode === 201) {
+            user = response.body;
+        }
+        expect(response.statusCode).toBe(201);
+        // expect(response.body).toEqual([]);
+    });
+
+it('With a GET api/user/{userId} request, we try to get the created record by its id (the created record is expected)'
+    , async () => {
+        const response = await request(server).get(`/api/users/${user?.id}`);
+        expect(response.statusCode).toBe(200);
+        expect((response.body).id).toEqual(user?.id);
+    });
+
+it('We try to update the created record with a PUT api/users/{userId}request (a response is expected containing an updated object with the same id)',
+    async () => {
+        const oldId=user?.id;
+        const response = await request(server).put(`/api/users/${oldId}`).send(JSON.stringify(changedUser));
+        if (response.statusCode === 200) {
+            user = response.body;
+        }
+        expect(response.statusCode).toBe(200);
+        expect((response.body).id).toEqual(oldId);
+    });
+
+it('With a DELETE api/users/{userId} request, we delete the created object by id (confirmation of successful deletion is expected)',
+    async () => {
+        const response = await request(server).delete(`/api/users/${user?.id}`);
+        expect(response.statusCode).toBe(204);
+    });
+
+it('With a GET api/users/{userId} request, we are trying to get a deleted object by id (expected answer is that there is no such object)'
+    , async () => {
+        const response = await request(server).get(`/api/users/${user?.id}`);
+        expect(response.statusCode).toBe(404);
+    });
